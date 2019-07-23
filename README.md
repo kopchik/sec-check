f73febb8dcc482018a8f6582aed31afda1b7f91a
Alexandre Kandalintsev <exe.sre@gmail.com>
2019-07-23 12:26:25 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # sec-check
+
+A sample of vulnerable code to evaluate linters.
+
+## Vulnerabilities and code quality issues
+
+1. SQL injection
+1. Unused imports
+1. hardcoded tokens
+1. Disabled SSL cert check
+1. Use of asserts
+1. Unused variables
+
+## Issues with dependencies
+
+1. Vulnerable packages
+1. Unpinned packages
+1. Malicious packages
+1. List doesn't include dependency of dependencies
+
+## TODO
+
+1. Have multiple requirements files to check discovery
+1. Add JS code
+1. Any way to test cookies?
+1. XSS?
---
+++ some_code.py
@@ -0,0 +1,28 @@
+# unused import
+import __future__
+
+SOME_HARDCODED_TOKEN = "dbdff73f-ca51-45f6-b1f1-3631f66164be"
+
+
+def validate_filename(filename):
+    # use of assert
+    assert not filename.startswith("/")
+
+
+def sqlinjection():
+    import sqlalchemy
+
+    engine = create_engine("sqlite:///bookstore.db")
+    user_name = input("user name?")
+    with engine.connect() as con:
+        con.execute("SELECT * FROM public.user WHERE user_name=%s" % user_name)
+
+
+def insecure_download():
+    import requests
+
+    return requests.get("https://google.com", verify=False)
+
+
+def unused_variables():
+    x = 1
